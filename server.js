ac3044143cc7f5b3c5873487a52ba3f4595942c8
Randy Olshen <rolshen@aol.com>
2020-04-23 19:52:04 -0700

    updated

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+// }
 
 app.use(routes)
 
